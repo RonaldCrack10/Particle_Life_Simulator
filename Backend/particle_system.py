dfb0906f3d3c3f3caf68843a20a61176346dcd94
RonaldCrack10 <rashidgaming6@gmail.com>
2025-12-17 09:57:44 +0100

    paricle system update

--- Backend/particle_system.py
+++ Backend/particle_system.py
@@ -14,16 +14,51 @@ class Particles:
             num_types: Anzahl der Typen, z.B. 4 für Rot/Grün/Blau/Gelb
     """
 
+  
+    """
+    Verwaltet alle Partikel als NumPy Arrays, ergänzt um @property
+    """
+    
     def __init__(self):
+            # keine Parameter
+            self._x = np.random.rand(NUM_PARTICLES) * WIDTH
+            self._y = np.random.rand(NUM_PARTICLES) * HEIGHT
+            self._velocity_x = np.zeros(NUM_PARTICLES)
+            self._velocity_y = np.zeros(NUM_PARTICLES)
+            self._types = np.random.randint(0, NUM_TYPES, NUM_PARTICLES)
         
-        # Position: Zufällig im Fenster verteilt (0 bis WIDTH/HEIGHT)
-        self.x = np.random.rand(NUM_PARTICLES) * WIDTH
-        self.y = np.random.rand(NUM_PARTICLES) * HEIGHT
+            # mit property
+            @property
+            def x(self) -> np.ndarray: 
+                return self._x
+            @x.setter
+            def x(self, value: np.ndarray) -> None: 
+                self._x = value
+            
+            @property
+            def y(self) -> np.ndarray: 
+                return self._y
+            @y.setter
+            def y(self, value: np.ndarray) -> None: 
+                self._y = value
         
-        # Geschwindigkeit: Startet bei 0 für alle Partikel
-        self.velocity_x = np.zeros(NUM_PARTICLES)
-        self.velocity_y = np.zeros(NUM_PARTICLES)
-        
-        # Typ: Zufällig zwischen 0 und num_types-1 (bestimmt Farbe & Verhalten)
-        self.types = np.random.randint(0, NUM_TYPES, NUM_PARTICLES)
-        
+            @property
+            def velocity_x(self) -> np.ndarray: 
+                return self._velocity_x
+            @velocity_x.setter
+            def velocity_x(self, value: np.ndarray) -> None: 
+                self._velocity_x = value
+            
+            @property
+            def velocity_y(self) -> np.ndarray: 
+                return self._velocity_y
+            @velocity_y.setter
+            def velocity_y(self, value: np.ndarray) -> None: 
+                self._velocity_y = value
+            
+            @property
+            def types(self) -> np.ndarray: 
+                return self._types
+            @types.setter
+            def types(self, value: np.ndarray) -> None: 
+                self._types = value
