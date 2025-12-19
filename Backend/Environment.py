59f5faf6289f1e546b13ab13b17944898943d8db
Michael Adzase <adzasemichael@pm.me>
2025-12-19 22:51:50 +0100

    merge konflikt sehr kompliziert

--- Backend/Environment.py
+++ Backend/Environment.py
@@ -15,6 +15,7 @@ class Environment:
                                                        [4, 1, 1, 1, -1])
         self._particles: Particles = Particles()
         self._checked_particles: np.ndarray= np.zeros(self._particles.shape)
+
 	@numba.jit(nopython = True)
     def check_interactions(self, position_x, position_y, radius, index) -> np.ndarray:
 		#positionen aller Particles im Radius herausfinden
@@ -30,12 +31,38 @@ class Environment:
 		#typen der Benachbarten Particles herausfinden
 		n_types: np.ndarray = self._particles.type[maske_n]
 		interactions = np.array([self._particles.types[index, x] for x in n_types])
-		return np.array([neighbours_x, neigbours_y, interactions])
+		
+		indices = np.where(mask)
+		return np.array([neighbours_x, neigbours_y, interactions, indices])
 
 	@numba.jit(nopython = True)
-	def calc_velocity(self, position_x, position_y, neigbours_x, neighbours_y) -> np.ndarray:
-		pass
-        pass
+	def calc_velocity(self, position_x: np.ndarray, position_y: np.ndarray, neigbours_x: np.ndarray, neighbours_y: np.ndarray, interactions: np.ndarray, index: int, indices: np.ndarray ) -> np.ndarray:
+		new_x: np.zeros(neighbour_x.shape[0])
+		new_y: np.zeros(neighbour_y.shape[0])
+		r1: np.ndarray = np.array([position_x, poition_y])
+		k: int = 1
+		m1: int = 1
+		m2: int = 1
+		t: float = 0.01
+		for i in range(neighbours_x.shape[0]):
+			r2 = np.array([neighbours_x, neighbours_y ])
+			r = r1 - r2
+			r_abs = np.sqrt((r1[0] - r2[0])**2 + (r1[1] - r2[1])**2)
+			r_norm: np.ndarray = r / r_abs
+			f1: np.ndarray = k * (np.prod(interactions)/r**2) * r_norm
+			f2: np.ndarray = f1 * -1
+			a1: np.ndarray = f1 / m1
+			a2: np.ndarray = f2 / m2
+			self._particles.velocity_x[index] = self._particles.velocity_x[index] + a1[0] * t
+			self._particles.velocity_y[index] = self._particles.velocity_y[index] + a1[1] * t
+			self._particles.velocity_x[indices[i]] = self._particles.velocity_x[indices[i]] + a2[0] * t
+			self._particles.velocity_y[indices[i]] = self._particles.velocity_y[indices[i]] + a2[1] * t
+			
+			#Position ändern
+			self._particles.x[index] = self._particles.x[index] + self._particles.velocity_x[index] * t
+            self._particles.y[index] = self._particles.y[index] + self._particles.velocity_x[index] * t
+			self._particles.x[indices[i]] = self._particles.x[indices[i]] + self._particles.velocity_x[indices[i]]  * t
+			self._particles.y[indices[i]] = self._particles.y[indices[i]] + self._particles.velocity_x[indices[i]]  * t
 
 
     def calc_friction(self, velocity_x: np.ndarray, velocity_y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -126,3 +153,4 @@ class Environment:
         pass
 
         
+
