84f1147bf9c41369ed8970422bf8939e0f93940f
RonaldCrack10 <rashidgaming6@gmail.com>
2026-02-13 20:22:12 +0100

    gui

--- Frontend/visualize.py
+++ Frontend/visualize.py
@@ -8,19 +8,8 @@ GUI - VisPy Visualisierung
 
 """
 
-
-
-# Diffusion function: adds Gaussian noise to particle positions
 env = Environment()
 
-# def diffuse(x, y, step_size=0.1):
-
-#     x += np.random.normal(scale=step_size, size=x.shape)
-
-#     y += np.random.normal(scale=step_size, size=y.shape)
-
-#     return x, y
- 
 canvas = scene.SceneCanvas(keys='interactive', show=True)
 view = canvas.central_widget.add_view()
 x_min, x_max = env.get_particles_x().min(), env.get_particles_x().max()
@@ -42,19 +31,9 @@ COLOR_MAP = {
 colors = np.array([COLOR_MAP[t] for t in env._particles.types])
 scatter.set_data(initial_pos, face_color=colors, size=10)
 view.add(scatter)
-# n_points = NUM_PARTICLES
 
-# x = np.random.normal(loc=0.0, scale=10.0, size=n_points)
-
-# y = np.random.normal(loc=0.0, scale=10.0, size=n_points)
- 
 def update(event):
 
-    # global x, y
-
-    # x, y = diffuse(x, y)
-
-    # scatter.set_data(np.array([x, y]).T, face_color='yellow', size=10)
 
     env.diffuse()
     new_pos = np.array([env.get_particles_x(), env.get_particles_y()]).T
