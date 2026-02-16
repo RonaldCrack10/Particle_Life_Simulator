2227b2738d94937e868398fd5220d8040971e951
RonaldCrack10 <rashidgaming6@gmail.com>
2026-02-16 21:13:40 +0100

    ..

--- Config/config.py
+++ Config/config.py
@@ -1,31 +1,33 @@
 import numpy as np
+from Backend.particle_system import Particles
+from Frontend.visualize import Visualize
+from Backend.Simulation import Simulation
 
 """
 Config - Parameter für Particle Life Simulator
 """
+def config():
+    #intanzierungen und configurierung
+    particles = Particles(x = np.random.normal(loc=0.0, scale=10.0, size=10000),
+                 y = np.random.normal(loc=0.0, scale=10.0, size=10000), 
+                 velocity_x = np.zeros(10000), 
+                 velocity_y = np.zeros(10000),  
+                 types = np.clip(np.rint(np.random.normal(loc=0, scale=1.0, size=10000)), 0, 4).astype(int),
+                 radius = 20)
+    simulation = Simulation(particles=particles)
+    visualize = Visualize(simulation=simulation, particles=particles)
+    visualize.start()
+    
 
 # Fenstergröße
 WIDTH, HEIGHT = 800, 800
 
 # Simulationsparameter
 NUM_PARTICLES = 2000  # Für Test weniger (später: 5000)
-PARTICLE_RADIUS = 5
-FRICTION = 0.95
-MAX_SPEED = 10
-INTERACTION_RADIUS = 100
-ATTRACTION_STRENGTH = 0.05
-RANDOM_FORCE_STRENGTH = 0.5
 
-# Farben und Partikeltypen
-COLORS = {
-    'RED': (255, 60, 60),
-    'GREEN': (60, 255, 60),
-    'BLUE': (60, 60, 255),
-    'YELLOW': (255, 255, 60)
-}
-COLOR_LIST = list(COLORS.keys())
-COLOR_VALUES = np.array(list(COLORS.values()))
-NUM_TYPES = len(COLOR_LIST)
+
+
+
 
 # Farben für VisPy (0.0-1.0 statt 0-255)
 COLORS_VISPY = np.array([
@@ -35,11 +37,4 @@ COLORS_VISPY = np.array([
     [1.0, 1.0, 0.2, 1.0],   # YELLOW
 ])
 
-# INTERACTION_MATRIX
-# Positive Werte = Abstoßung, Negative Werte = Anziehung
-INTERACTION_MATRIX = np.array([
-    [+1.0, -1.0, +0.5, -0.5],   # RED
-    [-1.0, +1.0, -0.5, +0.5],   # GREEN
-    [+0.5, -0.5, +1.0, -1.0],   # BLUE
-    [-0.5, +0.5, -1.0, +1.0],   # YELLOW
-])
+
